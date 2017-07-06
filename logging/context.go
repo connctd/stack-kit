0027547057a3714315e02760159bc48441050b2f
Till Klocke <till.klocke@gmail.com>
2017-07-06 10:28:50 +0200

    Added utility function to add context values to logging

---
+++ logging/context.go
@@ -0,0 +1,29 @@
+package logging
+
+import (
+	stdcontex "context"
+
+	"github.com/go-kit/kit/log"
+)
+
+var DefaultKeys []interface{} = []interface{}{
+	"X-Request-Id",
+	"X-Client-Id",
+	"X-Subject-Id",
+}
+
+func WithContext(logger log.Logger, ctx stdcontex.Context) log.Logger {
+	return WithContextKeys(logger, ctx, DefaultKeys)
+}
+
+func WithContextKeys(logger log.Logger, ctx stdcontex.Context, keys []interface{}) log.Logger {
+	keyvals := make(map[interface{}]interface{})
+
+	for _, key := range keys {
+		if val := ctx.Value(key); val != nil {
+			keyvals[key] = val
+		}
+	}
+
+	return log.With(logger, keyvals)
+}
